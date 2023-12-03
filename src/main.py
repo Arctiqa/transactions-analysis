ff64daccbd9da57884e504fe79f7e6983d887a6a
Arctiqa <sinitro@yandex.ru>
2023-12-03 20:31:47 +0800

    update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,18 @@
+from src.logger import setup_logging
+from src.views import compile_to_json
+
+logger = setup_logging()
+
+
 def main():
-    pass
+    file_path = 'operations.xls'
+    user_settings = 'user_settings.json'
+    # 13.08.2021 20:15:00
+    # 18.01.2019 20:15:00
+
+    user_input = input('ДД.ММ.ГГГГ HH:MM:SS -> ')
+    compile_to_json(file_path, user_input, user_settings)
+    print(f'Файл "user_information_set.json" создан в папке data')
 
 
 if __name__ == '__main__':
