258c583a05ebc1d0b565f67150bdb2e491b88ee8
Arctiqa <sinitro@yandex.ru>
2023-12-03 20:29:14 +0800

    add reports.py

--- src/reports.py
+++ src/reports.py
@@ -1,19 +1,27 @@
 from functools import wraps
 from typing import Optional, Callable
 import pandas as pd
-from datetime import datetime, timedelta
+from datetime import datetime
 import logging
 from dateutil.relativedelta import relativedelta
+import os
 
 logger = logging.getLogger(__name__)
 
 
-def spending_result(file_name: str = 'result.csv') -> Callable:
+def spending_result(file_name: str = 'result') -> Callable:
+    """
+    Декоратор для записи результата функции в csv файл
+    :param file_name: название записываемого файла
+    :return: результат выполняемой функции
+    """
+
     def wrapper(func):
         @wraps(func)
         def inner(*args, **kwargs):
             result = func(*args, **kwargs)
-            result.to_csv(fr'../data/{file_name}.csv', index=True)
+            path = f'{os.path.join(os.path.dirname(__file__), "..", "data", file_name)}.csv'
+            result.to_csv(path, index=True)
             logger.info(f'Result written to file - {file_name}')
             return result
 
@@ -23,17 +31,22 @@ def spending_result(file_name: str = 'result.csv') -> Callable:
 
 
 def gett_excel(operations_file):
-    transactions = pd.read_excel(operations_file, index_col=0)
+    path = f'{os.path.join(os.path.dirname(__file__), "..", "data", operations_file)}'
+    transactions = pd.read_excel(path, index_col=0)
     return transactions
 
 
-s = gett_excel(r'../data/operations.xls')
-
-
-@spending_result()
+@spending_result(file_name='result')
 def spending_by_category(transactions: pd.DataFrame,
                          category: str,
                          date: Optional[str] = None) -> pd.DataFrame:
+    """
+    Функция возвращает траты по заданной категории за последние 3 месяца (от переданной даты)
+    :param transactions: датафрейм с транзакциями
+    :param category: название категории
+    :param date: дата
+    :return:
+    """
     if date is None:
         current_date = datetime.now()
     else:
@@ -49,4 +62,7 @@ def spending_by_category(transactions: pd.DataFrame,
     return result
 
 
-spending_by_category(s, "Супермаркеты", "22.08.2020")
+# transactions_data = gett_excel('operations.xls')
+# categories = spending_by_category(transactions_data,
+#                                   "Супермаркеты",
+#                                   '17.07.2021')
